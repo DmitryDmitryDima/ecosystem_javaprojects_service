3845074aa9989bf94465b54915fd6e9ffa9e25b3
User <astakamokro@yandex.ru>
2025-11-16 11:37:29 +0300

    init commit

---
+++ src/main/java/com/ecosystem/projectsservice/javaprojects/JavaprojectsApplication.java
@@ -0,0 +1,13 @@
+package com.ecosystem.projectsservice.javaprojects;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaprojectsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaprojectsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecosystem/projectsservice/javaprojects/JavaprojectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecosystem.projectsservice.javaprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaprojectsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
