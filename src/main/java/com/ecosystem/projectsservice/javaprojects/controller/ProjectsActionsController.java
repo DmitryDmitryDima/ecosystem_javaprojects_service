092a5a7533adb7a3304f8a72d155ec002feac8e4
User <astakamokro@yandex.ru>
2025-12-09 20:51:07 +0300

    id based project api

--- src/main/java/com/ecosystem/projectsservice/javaprojects/controller/ProjectsActionsController.java
+++ src/main/java/com/ecosystem/projectsservice/javaprojects/controller/ProjectsActionsController.java
@@ -7,11 +7,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 /*
 actions - общие действия с существующим проектом
+
+
+Анализируется security context - проверяется, допущен ли тот, кто просматривает проект, к проекту
+Пропуски выдает хозяин проекта
  */
 
 @RestController
-@RequestMapping("/{name}/actions")
+@RequestMapping("/{id}/actions")
 public class ProjectsActionsController {
+
+
+
+
+
+
+
+
 
 
 
