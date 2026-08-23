5151089d4f1aed275d514fa1606ad1c2a5ed6898
admin <astakamokro@yandex.ru>
2026-08-23 11:10:37 +0300

    pure java  and spring part of framework isolation - preparing all to library form. Delete lombok elements from framework code 2

--- src/main/java/com/ecosystem/projectsservice/javaprojects/declarative_chain_framework_spring/model/idempotency/IdempotencyModelJpaEntity.java
+++ src/main/java/com/ecosystem/projectsservice/javaprojects/declarative_chain_framework_spring/model/idempotency/IdempotencyModelJpaEntity.java
@@ -2,7 +2,6 @@ package com.ecosystem.projectsservice.javaprojects.declarative_chain_framework_s
 
 import com.ecosystem.projectsservice.javaprojects.declarative_chain_framework.model.idempotency.IdempotencyModel;
 import jakarta.persistence.*;
-import lombok.*;
 import org.hibernate.annotations.UuidGenerator;
 import org.hibernate.id.uuid.UuidVersion7Strategy;
 
@@ -10,11 +9,6 @@ import java.util.UUID;
 
 
 @Entity
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@ToString
 @Table(name = "idempotency_registry")
 public class IdempotencyModelJpaEntity implements IdempotencyModel {
 
@@ -24,24 +18,82 @@ public class IdempotencyModelJpaEntity implements IdempotencyModel {
     @UuidGenerator(algorithm = UuidVersion7Strategy.class)
     private UUID id;
 
-
-
     // уникален в таблице
     @Column(unique = true)
     private UUID processId;
-
-
-
-
-
-
-
-
 
     @Override
     public UUID getProcessUUID() {
         return processId;
     }
+
+
+    @Override
+    public String toString(){
+
+        return "process id idempotency is "+processId;
+    }
+
+
+
+    public static IdempotencyModelJpaEntityBuilder builder() {
+        return new IdempotencyModelJpaEntityBuilder();
+    }
+
+
+    public void setId(final UUID id) {
+        this.id = id;
+    }
+
+
+    public void setProcessId(final UUID processId) {
+        this.processId = processId;
+    }
+
+
+    public IdempotencyModelJpaEntity() {
+    }
+
+
+    public IdempotencyModelJpaEntity(final UUID id, final UUID processId) {
+        this.id = id;
+        this.processId = processId;
+    }
+
+
+    public static class IdempotencyModelJpaEntityBuilder {
+
+        private UUID id;
+
+        private UUID processId;
+
+
+        IdempotencyModelJpaEntityBuilder() {
+        }
+
+
+        public IdempotencyModelJpaEntityBuilder id(final UUID id) {
+            this.id = id;
+            return this;
+        }
+
+
+        public IdempotencyModelJpaEntityBuilder processId(final UUID processId) {
+            this.processId = processId;
+            return this;
+        }
+
+
+        public IdempotencyModelJpaEntity build() {
+            return new IdempotencyModelJpaEntity(this.id, this.processId);
+        }
+
+
+
+    }
+
+
+
 
 
 }
--- src/main/java/com/ecosystem/projectsservice/javaprojects/declarative_chain_framework_spring/model/outbox/OutboxModelJpaEntity.java
+++ src/main/java/com/ecosystem/projectsservice/javaprojects/declarative_chain_framework_spring/model/outbox/OutboxModelJpaEntity.java
@@ -13,11 +13,6 @@ import java.util.UUID;
 
 // Jpa реализация
 @Entity
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@ToString
 @Table(name = "outbox_model")
 public class OutboxModelJpaEntity implements OutboxModel {
 
@@ -121,6 +116,241 @@ public class OutboxModelJpaEntity implements OutboxModel {
     @Override
     public Instant getLockedUntil() {
         return lockedUntil;
+    }
+
+
+
+    @Override
+    public String toString(){
+
+        return "outbox entity with "+outboxUUID+" related to process with "+processUUID;
+    }
+
+
+
+
+
+    public static OutboxModelJpaEntityBuilder builder() {
+        return new OutboxModelJpaEntityBuilder();
+    }
+
+
+    public void setOutboxUUID(final UUID outboxUUID) {
+        this.outboxUUID = outboxUUID;
+    }
+
+
+    public void setProcessUUID(final UUID processUUID) {
+        this.processUUID = processUUID;
+    }
+
+
+    public void setStatus(final OutboxStatus status) {
+        this.status = status;
+    }
+
+
+    public void setType(final String type) {
+        this.type = type;
+    }
+
+
+    public void setPayload(final String payload) {
+        this.payload = payload;
+    }
+
+
+    public void setLastUpdate(final Instant lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+
+    public void setReadExpiration(final Instant readExpiration) {
+        this.readExpiration = readExpiration;
+    }
+
+
+    public void setPerformanceLimitTime(final Long performanceLimitTime) {
+        this.performanceLimitTime = performanceLimitTime;
+    }
+
+
+    public void setAllReadVersion(final Long allReadVersion) {
+        this.allReadVersion = allReadVersion;
+    }
+
+
+    public void setAllReadProcessingVersion(final Long allReadProcessingVersion) {
+        this.allReadProcessingVersion = allReadProcessingVersion;
+    }
+
+
+    public void setMessage(final String message) {
+        this.message = message;
+    }
+
+
+    public void setCompensation(final boolean compensation) {
+        this.compensation = compensation;
+    }
+
+
+    public void setLockedUntil(final Instant lockedUntil) {
+        this.lockedUntil = lockedUntil;
+    }
+
+
+    public OutboxModelJpaEntity() {
+    }
+
+
+    public OutboxModelJpaEntity(final UUID outboxUUID,
+                                final UUID processUUID,
+                                final OutboxStatus status,
+                                final String type,
+                                final String payload,
+                                final Instant lastUpdate,
+                                final Instant readExpiration,
+                                final Long performanceLimitTime,
+                                final Long allReadVersion,
+                                final Long allReadProcessingVersion,
+                                final String message,
+                                final boolean compensation,
+                                final Instant lockedUntil) {
+        this.outboxUUID = outboxUUID;
+        this.processUUID = processUUID;
+        this.status = status;
+        this.type = type;
+        this.payload = payload;
+        this.lastUpdate = lastUpdate;
+        this.readExpiration = readExpiration;
+        this.performanceLimitTime = performanceLimitTime;
+        this.allReadVersion = allReadVersion;
+        this.allReadProcessingVersion = allReadProcessingVersion;
+        this.message = message;
+        this.compensation = compensation;
+        this.lockedUntil = lockedUntil;
+    }
+
+
+
+
+    public static class OutboxModelJpaEntityBuilder {
+
+        private UUID outboxUUID;
+
+        private UUID processUUID;
+
+        private OutboxStatus status;
+
+        private String type;
+
+        private String payload;
+
+        private Instant lastUpdate;
+
+        private Instant readExpiration;
+
+        private Long performanceLimitTime;
+
+        private Long allReadVersion;
+
+        private Long allReadProcessingVersion;
+
+        private String message;
+
+        private boolean compensation;
+
+        private Instant lockedUntil;
+
+
+        OutboxModelJpaEntityBuilder() {
+        }
+
+
+        public OutboxModelJpaEntityBuilder outboxUUID(final UUID outboxUUID) {
+            this.outboxUUID = outboxUUID;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder processUUID(final UUID processUUID) {
+            this.processUUID = processUUID;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder status(final OutboxStatus status) {
+            this.status = status;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder type(final String type) {
+            this.type = type;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder payload(final String payload) {
+            this.payload = payload;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder lastUpdate(final Instant lastUpdate) {
+            this.lastUpdate = lastUpdate;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder readExpiration(final Instant readExpiration) {
+            this.readExpiration = readExpiration;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder performanceLimitTime(final Long performanceLimitTime) {
+            this.performanceLimitTime = performanceLimitTime;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder allReadVersion(final Long allReadVersion) {
+            this.allReadVersion = allReadVersion;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder allReadProcessingVersion(final Long allReadProcessingVersion) {
+            this.allReadProcessingVersion = allReadProcessingVersion;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder message(final String message) {
+            this.message = message;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder compensation(final boolean compensation) {
+            this.compensation = compensation;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntityBuilder lockedUntil(final Instant lockedUntil) {
+            this.lockedUntil = lockedUntil;
+            return this;
+        }
+
+
+        public OutboxModelJpaEntity build() {
+            return new OutboxModelJpaEntity(this.outboxUUID, this.processUUID, this.status, this.type, this.payload, this.lastUpdate, this.readExpiration, this.performanceLimitTime, this.allReadVersion, this.allReadProcessingVersion, this.message, this.compensation, this.lockedUntil);
+        }
+
+
     }
 
 
